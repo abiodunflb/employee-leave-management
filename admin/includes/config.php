2fbb9bc8090826f52a7caea608aeb5a08f4dd782
unknown <abiodun_flb@yahoo.com>
2021-05-21 20:05:12 +0100

    added admin db

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,16 +1,12 @@
-<?php 
+<?php
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','elms');
+define('DB_HOST', 'us-cdbr-east-03.cleardb.com');
+define('DB_USER', 'b63e33cb97d614');
+define('DB_PASS', 'e89b8fb0');
+define('DB_NAME', 'heroku_a14305119a36ca7');
 // Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+try {
+    $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+} catch (PDOException $e) {
+    exit("Error: " . $e->getMessage());
 }
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
-}
-?>
